23dfbdcde5d4f9bae3e28e3b59245fb397fde631
aradhyacp <cparadhya@proton.me>
2025-07-16 18:56:07 +0530

    added toast

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,38 +1,58 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 
 function App() {
-  const [counter,setCounter]= useState(5);
-  const [classColor,setclassColor] = useState('');
-  const addValue =()=>{
+  const [counter, setCounter] = useState(5);
+  const [classColor, setclassColor] = useState("");
+  const addValue = () => {
     console.log(counter);
-    setCounter(counter+1);
-    setclassColor('green');
-  }
+    setCounter(counter + 1);
+    setclassColor("green");
+    Toastify({
+      text: "Added +1",
+      duration: 1000,
+      style: {
+        background:
+          "linear-gradient(to right, rgb(0, 176, 155), rgb(150, 201, 61))",
+      },
+    }).showToast();
+  };
 
-  const minusValue = () =>{
-    setCounter(counter-1);
-    setclassColor('red');
-  }
+  const minusValue = () => {
+    setCounter(counter - 1);
+    setclassColor("red");
+    Toastify({
+      text: "Reduced -1",
+      duration: 1000,
+      style: {
+        background: "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))",
+      }
+    }).showToast();
+  };
 
-  useEffect(()=>{
-    if(classColor){
+  useEffect(() => {
+    if (classColor) {
       const timeout = setTimeout(() => {
-        setclassColor('')
+        setclassColor("");
       }, 400);
       return () => clearTimeout(timeout);
     }
-  },[classColor])
+  }, [classColor]);
 
   return (
     <>
-    <h1>Counter example</h1>
-    <h2>Current value: <span className={classColor}>{counter}</span></h2>
-    <div className="button-container"><button onClick={addValue}>Increment</button>
-    <button onClick={minusValue}>Decrement</button></div>
-
+      <h1>Counter example</h1>
+      <h2>
+        Current value: <span className={classColor}>{counter}</span>
+      </h2>
+      <div className="button-container">
+        <button onClick={addValue}>Increment</button>
+        <button onClick={minusValue}>Decrement</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
