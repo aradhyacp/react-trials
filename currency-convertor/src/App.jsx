d00a9a6f3d0f78415bfa64bb5c5600c11182a773
aradhyacp <cparadhya@proton.me>
2025-07-19 00:09:19 +0530

    currency-convertor all changes

--- currency-convertor/src/App.jsx
+++ currency-convertor/src/App.jsx
@@ -1,11 +1,78 @@
+import { useState } from 'react'
 import './App.css'
+import InputBox from './components/InputBox'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
+  const [amount,setAmount] = useState(0);
+  const [from,setFrom] = useState("usd");
+  const [to,setTo] = useState("inr")
+  const [result,setResult] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo);
+  const swap = () =>{
+    setFrom(to)
+    setTo(from)
+  }
+  const convert = () =>{
+    if (!currencyInfo[to]) return;
+    setResult(amount*currencyInfo[to])
+  }
+
   return (
-    <div>
-      <div className="text-3xl bg-blue-300">Hello test</div>
-    </div>
-  )
+        <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+              backgroundImage: `url('https://images.pexels.com/photos/1006060/pexels-photo-1006060.jpeg?cs=srgb&dl=pexels-steve-1006060.jpg&fm=jpg&w=5184&h=2968&_gl=1*13omp1n*_ga*MTA4NjQ1NzA4Ny4xNzUyODYyMjY0*_ga_8JE65Q40S6*czE3NTI4NjIyNjMkbzEkZzEkdDE3NTI4NjIyOTQkajI5JGwwJGgw')`
+            }}
+        >
+            <div className="w-full">
+                <div className="w-[50%] mx-auto border border-gray-300 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            convert()
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                                label="From"
+                                amount={amount}
+                                currencyOptions={options}
+                                onCurrencyChange={(currency)=>setFrom(currency)}
+                                selectCurrency={from}
+                                onAmountChange={(amount)=>setAmount(amount)}
+                                
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                onClick={swap}
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                                label="To"
+                                amount={result}
+                                currencyOptions={options}
+                                onCurrencyChange={(currency)=>setTo(currency)}
+                                amountDisabled={true}
+                                selectCurrency={to}
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                            Convert {from.toUpperCase()} to {to.toUpperCase()}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App
---
+++ currency-convertor/src/components/InputBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useId } from 'react'
+
+const InputBox = ({label,amount,onAmountChange,onCurrencyChange,currencyOptions=[],selectCurrency="usd",amountDisabled=false,currencyDisabled=false,className=""}) => {
+    const amountInputId = useId()
+  return (
+    <div className={`bg-white p-3 flex rounded-lg ${className}`}>
+        <div className="w-1/2 ">
+        <label className='text-black/40 mb-2 inline-block' htmlFor={amountInputId}>{label}</label>
+        <input id={amountInputId} type="number" className='w-full outline-none bg-transparent py-1.5' placeholder='Amount' disabled={amountDisabled} value={amount} onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}/></div>
+        <div className="flex w-1/2 flex-wrap justify-end text-right">
+            <span className='text-black/40 mb-2 w-full'>Currency Type</span>
+            <select className='rounded-lg p-5 bg-gray-100 cursor-pointer outline-none' value={selectCurrency} onChange={(e)=>onCurrencyChange&&onCurrencyChange(e.target.value)} disabled={currencyDisabled}>
+                {currencyOptions.map((c)=>(
+                    <option key={c} value={c}>{c}</option>
+                ),)}
+            </select>
+        </div>
+    </div>
+  )
+}
+
+export default InputBox
