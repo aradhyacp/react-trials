12eba0b78212c1d829c6b770803d70e325d4fff7
aradhyacp <cparadhya@proton.me>
2025-07-18 13:15:19 +0530

    light-dark-mode added header

---
+++ light-dark-mode/src/components/Header.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="flex header-container items-center flex-row justify-between">
+      <div className="flex items-center gap-4 header-logo-container px-10 py-5">
+        <div class="size-6">
+          <svg
+            viewBox="0 0 48 48"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M24 4C25.7818 14.2173 33.7827 22.2182 44 24C33.7827 25.7818 25.7818 33.7827 24 44C22.2182 33.7827 14.2173 25.7818 4 24C14.2173 22.2182 22.2182 14.2173 24 4Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </div>
+        <div className="header-text text-[22px]">Project Starter</div>
+      </div>
+      <div className="flex header-button-container gap-12 px-10 py-5 font-medium flex-row items-center">
+        <div className="cursor-pointer header-nav">Home</div>
+        <div className="cursor-pointer header-nav">About</div>
+        <div className="cursor-pointer header-nav">Contact</div>
+        <div className="cursor-pointer dark-mode rounded-lg">
+          <div
+            class="text-[#141414]"
+            data-icon="Sun"
+            data-size="20px"
+            data-weight="regular"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+            >
+              <path d="M120,40V16a8,8,0,0,1,16,0V40a8,8,0,0,1-16,0Zm72,88a64,64,0,1,1-64-64A64.07,64.07,0,0,1,192,128Zm-16,0a48,48,0,1,0-48,48A48.05,48.05,0,0,0,176,128ZM58.34,69.66A8,8,0,0,0,69.66,58.34l-16-16A8,8,0,0,0,42.34,53.66Zm0,116.68-16,16a8,8,0,0,0,11.32,11.32l16-16a8,8,0,0,0-11.32-11.32ZM192,72a8,8,0,0,0,5.66-2.34l16-16a8,8,0,0,0-11.32-11.32l-16,16A8,8,0,0,0,192,72Zm5.66,114.34a8,8,0,0,0-11.32,11.32l16,16a8,8,0,0,0,11.32-11.32ZM48,128a8,8,0,0,0-8-8H16a8,8,0,0,0,0,16H40A8,8,0,0,0,48,128Zm80,80a8,8,0,0,0-8,8v24a8,8,0,0,0,16,0V216A8,8,0,0,0,128,208Zm112-88H216a8,8,0,0,0,0,16h24a8,8,0,0,0,0-16Z"></path>
+            </svg>
+          </div>
+        </div>
+        <div className="cursor-pointer pfp size-11 bg-no-repeat aspect-square bg-cover rounded-full bg-center"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
