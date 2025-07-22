9697b86a4a63cd2be87753ae2da202313cb34cc8
aradhyacp <cparadhya@proton.me>
2025-07-22 12:16:45 +0530

    Router-test added footer

--- router-test/src/pages/About.jsx
+++ router-test/src/pages/About.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <div className="flex justify-center items-center">
+            <div className="w-[800px]"><img src="https://www.12dimensionsapp.in/assets/img/aboutarvaya.png" alt="" /></div>
+            <div className="flex flex-col gap-5 w-[30%]">
+                <div className="text-4xl font-black">React development is carried out by passionate developers</div>
+                <div className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, culpa. Inventore alias id tempore blanditiis laborum officia ex! Facilis, incidunt nostrum? Quis illum, nihil numquam voluptas quae harum commodi atque. Ex, doloremque natus.</div>
+                <div className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta assumenda ipsa sequi molestiae reiciendis sed.</div>
+            </div>
+        </div>
+    </div>
   )
 }
 
