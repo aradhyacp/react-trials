1b8e069e9fcc19a80a92ffa62158e714635f5de7
aradhyacp <cparadhya@proton.me>
2025-07-22 14:01:59 +0530

    Router-test New way of routing

--- router-test/src/routes/AppRoutes.jsx
+++ router-test/src/routes/AppRoutes.jsx
@@ -1,27 +1,53 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
-import Github from "../pages/Github";
+import Github, { githubInfoLoader } from "../pages/Github";
 import MainLayout from "../layout/MainLayout";
 import NotFound from "../pages/NotFound";
 import User from "../pages/User";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route
+          path="/github"
+          element={<Github />}
+          loader={githubInfoLoader}
+        ></Route>
+        <Route path="/user/:id" element={<User />}></Route>
+      </Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </>
+  )
+);
 const AppRoutes = () => {
   return (
-    <Router>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/github" element={<Github />}></Route>
-          <Route path="/user/:id" element={<User/>}></Route>
-        </Route>
-        <Route path="*" element={<NotFound/>}></Route>
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route element={<MainLayout />}>
+    //       <Route path="/" element={<Home />}></Route>
+    //       <Route path="/about" element={<About />}></Route>
+    //       <Route path="/contact" element={<Contact />}></Route>
+    //       <Route path="/github" element={<Github />} loader={githubInfoLoader}></Route>
+    //       <Route path="/user/:id" element={<User/>}></Route>
+    //     </Route>
+    //     <Route path="*" element={<NotFound/>}></Route>
+    //   </Routes>
+    // </Router>
+    <RouterProvider router={router} />
   );
 };
 
