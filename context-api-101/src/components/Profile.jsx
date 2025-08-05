129126114fd2eecf37863ba2301900ba447483c9
aradhyacp <cparadhya@proton.me>
2025-08-06 00:27:30 +0530

    Context-api-101 ui change added profile

--- context-api-101/src/components/Profile.jsx
+++ context-api-101/src/components/Profile.jsx
@@ -1,20 +1,35 @@
-import React, { useContext } from 'react'
-import UserContext from '../context/UserContext'
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 const Profile = () => {
-    const {user,setUser} = useContext(UserContext)
-    const handleLogout = () =>{
-        setUser(null)
-    }
+  const { user, setUser } = useContext(UserContext);
+  const handleLogout = () => {
+    setUser(null);
+  };
   return (
-    <div className="text-black">
-    <div>{user?<h1>Welcome {user.userName}</h1>: <h1>please login</h1>}</div>
+    <div className="min-h-screen bg-[#171122] pt-13">
+      {/* <div>{user?<h1>Welcome {user.userName}</h1>: <h1>please login</h1>}</div>
     {user && (
   <button onClick={handleLogout}>Logout</button>
-)}
-
+)} */}
+      <div className="flex items-center flex-col gap-5 w-fit mx-auto">
+        <div className="size-50 mt-6">
+          <img
+            src="https://github.com/aradhyacp/react-trials/blob/main/light-dark-mode/public/defaultpfp.jpeg?raw=true"
+            alt="pfp"
+            className="rounded-full"
+          />
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <div className="text-2xl font-bold">@ {user.userName}</div>
+          <div className="text-[#a493c8] text-base font-normal leading-normal text-center">
+            Password: {user.password}
+          </div>
+        </div>
+        <div className="px-4 bg-[#7b47eb] h-10 flex items-center font-bold rounded-lg cursor-pointer" onClick={handleLogout}>Logout</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
