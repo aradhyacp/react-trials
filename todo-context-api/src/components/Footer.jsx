ea096ce2a57fa6e7374e6ee159c9960f85e088b7
aradhyacp <cparadhya@proton.me>
2025-08-13 23:44:25 +0530

    todo-context-api ui v2.2

--- todo-context-api/src/components/Footer.jsx
+++ todo-context-api/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='border-t-2 border-t-[#3a3827] mt-5 flex justify-center'>
-        <div className="text-[#bbb89b] mt-3 mb-3">Buit with ❤️ by ACP.IO</div>
+        <div className="text-[#bbb89b] mt-3 mb-3">Built with ❤️ by ACP.IO</div>
     </div>
   )
 }
--- todo-context-api/src/components/TodoElement.jsx
+++ todo-context-api/src/components/TodoElement.jsx
@@ -24,10 +24,34 @@ const TodoElement = ({ todo }) => {
         } items-center justify-between`}
       >
         <div className="flex flex-col">
-          <div className="flex flex-row">
+          <div className="flex flex-row items-center">
             <input
               type="checkbox"
-              className="cursor-pointer mr-2"
+              className={`
+    w-5 h-5
+    appearance-none
+    rounded
+    border-2
+    border-[#55523a]
+    checked:border-transparent
+    checked:bg-[#f9e106]
+    bg-[#181811]
+    cursor-pointer
+    transition-all
+    duration-200
+    relative
+    flex
+    justify-center
+    items-center
+    shrink-0
+    after:content-['✓']
+    after:text-[#181811]
+    after:text-sm
+    after:font-bold
+    after:scale-0
+    checked:after:scale-100
+    mr-2
+  `}
               checked={todo.completed}
               onChange={toggleCompleted}
             />
@@ -41,23 +65,64 @@ const TodoElement = ({ todo }) => {
               readOnly={!isTodoEditable}
             />
           </div>
-           <div className={todo.completed?`text-green-400`:`text-red-300`}>{todo.completed ? "Finished" : "Pending"}</div>
+          <div className={todo.completed ? `text-green-400` : `text-red-300`}>
+            {todo.completed ? "Finished" : "Pending"}
+          </div>
         </div>
         {/* Edit, Save Button */}
         <div className="flex flex-row gap-4">
-        <button
-          className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
-          onClick={() => {
-            if (todo.completed) return;
+          <button
+            className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+            onClick={() => {
+              if (todo.completed) return;
 
-            if (isTodoEditable) {
-              editTodo();
-            } else setIsTodoEditable((prev) => !prev);
-          }}
-          disabled={todo.completed}
-        >
-          {isTodoEditable ? (
-            <div className="size-5 text-[#2fc755] cursor-pointer">
+              if (isTodoEditable) {
+                editTodo();
+              } else setIsTodoEditable((prev) => !prev);
+            }}
+            disabled={todo.completed}
+          >
+            {isTodoEditable ? (
+              <div className="size-5 text-[#2fc755] cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-save-icon lucide-save"
+                >
+                  <path d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z" />
+                  <path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7" />
+                  <path d="M7 3v4a1 1 0 0 0 1 1h7" />
+                </svg>
+              </div>
+            ) : (
+              <div className="size-5 text-black cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-pencil-icon lucide-pencil"
+                >
+                  <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+                  <path d="m15 5 4 4" />
+                </svg>
+              </div>
+            )}
+          </button>
+          {/* Delete Todo Button */}
+          <button
+            className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+            onClick={() => deleteTodo(todo.id)}
+          >
+            <div className="size-5 text-red-500 cursor-pointer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -66,54 +131,15 @@ const TodoElement = ({ todo }) => {
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="lucide lucide-save-icon lucide-save"
+                class="lucide lucide-delete-icon lucide-delete"
               >
-                <path d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z" />
-                <path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7" />
-                <path d="M7 3v4a1 1 0 0 0 1 1h7" />
+                <path d="M10 5a2 2 0 0 0-1.344.519l-6.328 5.74a1 1 0 0 0 0 1.481l6.328 5.741A2 2 0 0 0 10 19h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z" />
+                <path d="m12 9 6 6" />
+                <path d="m18 9-6 6" />
               </svg>
             </div>
-          ) : (
-            <div className="size-5 text-black cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-pencil-icon lucide-pencil"
-              >
-                <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
-                <path d="m15 5 4 4" />
-              </svg>
-            </div>
-          )}
-        </button>
-        {/* Delete Todo Button */}
-        <button
-          className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
-          onClick={() => deleteTodo(todo.id)}
-        >
-          <div className="size-5 text-red-500 cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-delete-icon lucide-delete"
-            >
-              <path d="M10 5a2 2 0 0 0-1.344.519l-6.328 5.74a1 1 0 0 0 0 1.481l6.328 5.741A2 2 0 0 0 10 19h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z" />
-              <path d="m12 9 6 6" />
-              <path d="m18 9-6 6" />
-            </svg>
-          </div>
-        </button>
-       </div>
+          </button>
+        </div>
       </div>
     </div>
   );
